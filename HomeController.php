36f47e73644f1010842e2e2ef460ae6b20fa8347
ihwanridho <87596352+ihwanridho@users.noreply.github.com>
2021-07-18 15:19:40 +0700

    Add files via upload

---
+++ HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\sysuser;
+
+class HomeController extends Controller
+{
+    public function index(Request $request)
+    }
+        return view('welcome');
+    }
+
+    public function login(Request $request)
+    {
+        $session = $request->session()->exists('userid');
+        if(!$session){
+            return view('auth.login');
+        }else{
+            return redirect('/');
+        }
+    }
